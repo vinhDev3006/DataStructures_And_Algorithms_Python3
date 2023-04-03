188e6c8e47759379e4ce4b5defbe9f29137f7645
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-04-03 10:11:27 +0700

    Update BinarySearch.py

--- Algorithms/Search/BinarySearch.py
+++ Algorithms/Search/BinarySearch.py
@@ -1,3 +1,23 @@
+"""
+Binary search is an algorithm used to efficiently search for an element in a sorted list or array. The basic idea behind binary search is to divide the search range in 
+half with each iteration, until the target element is found or it is determined that the element is not present in the list.
+
+Here's how binary search works:
+    1. Start with the middle element of the list.
+    2. If the middle element is the target element, stop the search and return its index.
+    3. If the middle element is greater than the target element, search the left half of the list.
+    4. If the middle element is less than the target element, search the right half of the list.
+    5. Repeat steps until the target element is found or it is determined that it is not present in the list.
+    
+Binary search has a time complexity of O(log n), which makes it very efficient for large lists. However, it only works on sorted lists, 
+so if the list is unsorted, it must be sorted first.
+
+Some notable examples where binary search is used include:
+    1. Searching for a word in a dictionary - a dictionary is typically sorted alphabetically, so binary search can be used to quickly find a word.
+    2. Searching for a number in a phonebook - similar to searching for a word in a dictionary, binary search can be used to quickly find a phone number in a sorted list.
+    3. Finding an element in a large database - if the database is sorted, binary search can be used to quickly locate a specific record.
+    4. Implementing autocomplete functionality in a text editor - when a user types a few characters, binary search can be used to quickly find a list of possible matches based on a sorted dictionary of words.
+"""
 import unittest
 
 def binary_search(arr, target) -> str:
